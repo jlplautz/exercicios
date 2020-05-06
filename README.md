9444f40f65decb6b511249771660f0741c19bbe5
61246880 <jorge.plautz@gmail.com>
2020-05-06 00:09:45 -0300

    exercicios funcionais - decorator modificador e decorator_complexo
    close #5

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # exercicios
 Projeto para guardar os exercicios da lives e tambem outros proprios
 
+![Python application](https://github.com/jlplautz/exercicios/workflows/Python%20application/badge.svg)
+[![Updates](https://pyup.io/repos/github/jlplautz/exercicios/shield.svg)](https://pyup.io/repos/github/jlplautz/exercicios/)
+[![Python 3](https://pyup.io/repos/github/jlplautz/exercicios/python-3-shield.svg)](https://pyup.io/repos/github/jlplautz/exercicios/)
+
+# Exercicios programação funcional
+
+- ordenação
+- decorator_modificador
+- decorator_marcador
+- decirator_complexo
--- funcional/decorator_modificador.py
+++ funcional/decorator_modificador.py
@@ -1,23 +1,49 @@
 from time import time
+from functools import wraps
+
+def envolver(funcao_envolvida):
+    def editar_metadados(funcao_envoltoria):
+        funcao_envoltoria.__name__ = funcao_envolvida.__name__
+        funcao_envoltoria.__doc__ = funcao_envolvida.__doc__
+        return funcao_envoltoria
+
+    return editar_metadados
 
 
 def modificar(funcao):
     print('Trocar função original por outra')
 
-    def funcao_modificada(*arg, **kwargs):
+    @wraps(funcao)
+    def funcao_envoltoria(*arg, **kwargs):
+        """"
+        Funcao envoltoria
+        """
         tempo_inicial = time()
         resultado = funcao(*arg, **kwargs)
         tempo_final = time()
         print(tempo_final - tempo_inicial)
         return resultado
-    return funcao_modificada
+
+    return funcao_envoltoria
 
 
 @modificar
 def soma(a, b):
+    """"
+    função soma
+    """
     return a+b
 
 
 print(soma(1, 2))
 print(soma(1000000000, 2*1000))
 print(soma.__name__)
+
+
+def modulo(a, b):
+    if a > b:
+        return a - b
+    return b - a
+
+print(modulo(1,2))
+print(modulo(2,1))
